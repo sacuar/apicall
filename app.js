a44e21171b6d32276014319db0123de393adb0bd
Muhammed Sacuar Hussain <muhammed.hussain@stud.uni-due.de>
2023-07-12 01:58:43 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Define routes
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
 
